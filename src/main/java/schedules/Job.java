d8f73c9ed5dd2e372fabcaa4d3672775c77b1a29
slam-shark <dmytromanuk@gmail.com>
2013-09-21 22:27:27 +0300

    Jobs Interface

---
+++ src/main/java/schedules/Job.java
@@ -0,0 +1,35 @@
+/*
+ * Scheduling job
+ * Provides implementation of Comparable's method: compareTo
+ * Defines  getSignificance() method to compare different jobs
+ */
+package main.java.schedules;
+
+public abstract class Job implements Comparable<Job> {
+    final int weight;
+    final int length;
+
+    Job(int w, int l) {
+	weight = w;
+	length = l;
+    }
+
+    public abstract double getSignificance();
+    public abstract String getDescription();
+
+    @Override
+    public int compareTo(Job j) {
+	double signThis = getSignificance();
+	double signThat = j.getSignificance();
+	if (signThis < signThat)
+	    return -1;
+	if (signThis > signThat)
+	    return 1;
+	if (weight < j.weight)
+	    return -1;
+	if (weight > j.weight)
+	    return 1;
+	return 0;
+
+    }
+}
