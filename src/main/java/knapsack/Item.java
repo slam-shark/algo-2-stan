5907033b72967536c969776b98982b4f189130a6
slam-shark <dmytromanuk@gmail.com>
2013-09-27 23:18:41 +0300

    small fix

--- src/main/java/knapsack/Item.java
+++ src/main/java/knapsack/Item.java
@@ -8,9 +8,9 @@ public class Item {
 
     public Item(int value, int weight, int name) {
 	if (weight <= 0)
-	    throw new IllegalArgumentException("weight of item should be positive ");
+	    throw new IllegalArgumentException("weight of item should be positive");
 	if (value <= 0)
-	    throw new IllegalArgumentException("value of item should be positive ");
+	    throw new IllegalArgumentException("value of item should be positive");
 	this.value = value;
 	this.weight = weight;
 	this.lbl = name;
