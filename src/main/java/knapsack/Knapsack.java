406a8f93ff321e3de64b0af20b0203ef50eba6a6
slam-shark <dmytromanuk@gmail.com>
2013-09-29 01:15:00 +0300

    update Knapsack method types

--- src/main/java/knapsack/Knapsack.java
+++ src/main/java/knapsack/Knapsack.java
@@ -1,12 +1,31 @@
 package main.java.knapsack;
 
-import java.util.Deque;
+import java.util.NavigableSet;
 
 public interface Knapsack {
 
     void getArray(String filePath);
+
+    /**
+     * 
+     * @return total value of items in Knapsack
+     */
     public int getTotalResult();
-    public Deque<Item> getSelectedItems();
+    
+    /**
+     * 
+     * @return list of numbers of selected items in ASC order
+     */
+    public NavigableSet<? extends Item> getSelectedItems();
+
+    /**
+     * explicit calculation
+     */
     public void calculate();
+
+    /**
+     * 
+     * @return list of numbers of selected items in ASC order
+     */
     public String getSelectedNumbers();
 }
