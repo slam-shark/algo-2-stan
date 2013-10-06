55257634efe07a94293f297dd363125f06b971f4
slam-shark <dmytromanuk@gmail.com>
2013-10-06 16:41:17 +0300

    created Vertex, Edge, Graph (G={V,E}) for shortest paths

---
+++ src/shortestPath/Edge.java
@@ -0,0 +1,46 @@
+package shortestPath;
+
+public class Edge {
+    private final Vertex source;
+    private final Vertex destination;
+    private int weight;
+
+    public Edge(Vertex src, Vertex dst, int weight) {
+	this.weight = weight;
+	if (src.equals(dst))
+	    throw new IllegalArgumentException("oops, verticles are same");
+	if (src == null || dst == null)
+	    throw new IllegalArgumentException("Both vertices are required");
+	source = src;
+	destination = dst;
+	src.addEdge(this);
+	dst.addEdge(this);
+
+    }
+
+    public int getWeight() {
+	return weight;
+    }
+
+    public void setWeight(int newWeight) {
+	weight = newWeight;
+    }
+
+    public void changeWeightBy(int delta) {
+	weight += delta;
+    }
+
+    public Vertex getSource() {
+	return source;
+    }
+
+    public Vertex getDestination() {
+	return destination;
+    }
+
+    @Override
+    public String toString() {
+	return String.format("(%4s ->%4s) |%4s", source, destination, weight);
+    }
+
+}
---
+++ src/shortestPath/Vertex.java
@@ -0,0 +1,78 @@
+package shortestPath;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Vertex {
+
+    private final int label;
+    private final Set<Edge> edges = new HashSet<Edge>();
+    private int minDistance = Integer.MAX_VALUE;
+    private Vertex previous = null;
+
+    public Vertex(int label) {
+	this.label = label;
+    }
+
+    public void addEdge(Edge e) {
+	edges.add(e);
+    }
+
+    public int getLabel() {
+	return label;
+
+    }
+
+    public Set<Edge> getEdges() {
+	return edges;
+    }
+
+    public int getMinDistance() {
+	return minDistance;
+    }
+
+    public void setMinDistance(int minDistance) {
+	this.minDistance = minDistance;
+    }
+
+    public void changeMinDistanceBy(int delta) {
+	this.minDistance += delta;
+    }
+
+    public void setDistanceToMax() {
+	this.minDistance = Integer.MAX_VALUE;
+    }
+
+    public Vertex getPrevious() {
+	return previous;
+    }
+
+    public void setPrevious(Vertex previous) {
+	this.previous = previous;
+    }
+
+    public void removePrevious() {
+	this.previous = null;
+    }
+
+    private void removeEdge(Edge e) {
+	edges.remove(e);
+    }
+
+    public void removeTails() {
+	for (Edge e : edges) {
+	    e.getDestination().removeEdge(e);
+	}
+    }
+
+    public void reset() {
+	removePrevious();
+	setDistanceToMax();
+    }
+
+    @Override
+    public String toString() {
+	return "" + label + "{" + getMinDistance() + "}";
+    }
+
+}
