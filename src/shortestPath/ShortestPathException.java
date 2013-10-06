2c0575429253761d605bc8ec674d929f086f94bf
slam-shark <dmytromanuk@gmail.com>
2013-10-06 16:43:05 +0300

    created ShortestPathException

---
+++ src/shortestPath/ShortestPathException.java
@@ -0,0 +1,10 @@
+package shortestPath;
+
+public class ShortestPathException extends Exception {
+
+    private static final long serialVersionUID = -2155988850761224437L;
+
+    public ShortestPathException(String msg) {
+	super(msg);
+    }
+}
